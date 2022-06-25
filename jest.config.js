e0f7098dcd87e01be1efea61cd2df4e5d1c70307
Higor Silva <higor.junio@hotmail.com>
2022-06-25 11:27:51 -0300

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!**/test/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
