fa88f9d0a2b7af6011801211ad5113f38bd206b1
Higor Silva <higor.junio@hotmail.com>
2022-07-02 12:50:48 -0300

    refactor: register validations and messages

--- src/helpers/ErrorMessages.ts
+++ src/helpers/ErrorMessages.ts
@@ -1,1 +1,15 @@
 export const requiredMessage = 'Required field'
+export const invalidEmailMessage = 'Invalid e-mail'
+
+export const fieldSizeMessage = (min?: number | null, max?: number): string => {
+  if (min && max) {
+    return `Field needs to be between ${min} and ${max} characters long`
+  }
+  if (min) {
+    return `Field needs to be at least ${min} characters long`
+  }
+  if (max) {
+    return `Field needs to be less than ${max} characters long`
+  }
+  return ''
+}
--- src/middlewares/Requests/UserRequests.ts
+++ src/middlewares/Requests/UserRequests.ts
@@ -1,11 +1,15 @@
-import { requiredMessage } from '@/helpers/ErrorMessages'
+import { fieldSizeMessage, invalidEmailMessage, requiredMessage } from '@/helpers/ErrorMessages'
 import { body } from 'express-validator'
 
 export const RegisterUserRequest = [
   body('username')
-    .notEmpty().withMessage(requiredMessage).bail(),
+    .notEmpty().withMessage(requiredMessage).bail()
+    .isLength({ min: 5, max: 25 }).withMessage(fieldSizeMessage(5, 25)),
   body('email')
-    .notEmpty().withMessage(requiredMessage).bail(),
+    .notEmpty().withMessage(requiredMessage).bail()
+    .normalizeEmail()
+    .isEmail().withMessage(invalidEmailMessage),
   body('password')
     .notEmpty().withMessage(requiredMessage).bail()
+    .isLength({ min: 5, max: 25 }).withMessage(fieldSizeMessage(5, 25))
 ]
