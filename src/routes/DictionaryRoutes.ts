1ecfba36bced124c89896fcfebd8d84fc57765af
Higor Silva <higor.junio@hotmail.com>
2022-07-04 21:04:21 -0300

    feat: add admin middleware to store term route

--- src/routes/DictionaryRoutes.ts
+++ src/routes/DictionaryRoutes.ts
@@ -1,3 +1,4 @@
+import { authorize } from '@/middlewares/Auth'
 import { AddDefinitionRequest } from '@/middlewares/Requests/DictionaryRequests'
 import { validateRequest } from '@/middlewares/ValidateRequest'
 import express from 'express'
@@ -5,6 +6,6 @@ import { store } from '../controllers/DictionaryController'
 
 const router = express.Router()
 
-router.post('/store', AddDefinitionRequest, validateRequest, store)
+router.post('/store', authorize('admin'), AddDefinitionRequest, validateRequest, store)
 
 export { router as dictionaryRoutes }
