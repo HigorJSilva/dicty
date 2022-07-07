01f11794db10618f5e6854bf6b92845f10f594e3
Higor Silva <higor.junio@hotmail.com>
2022-07-06 21:14:39 -0300

    refactor: add AuthenticatedUserRequest to AddDefinitionRequest

--- src/middlewares/Requests/DictionaryRequests.ts
+++ src/middlewares/Requests/DictionaryRequests.ts
@@ -1,7 +1,18 @@
 import { fieldSizeMessage, requiredMessage } from '@/helpers/ErrorMessages'
 import { body } from 'express-validator'
+import { AuthenticatedUserRequest } from './AuthRequest'
 
 export const AddDefinitionRequest = [
+  ...AuthenticatedUserRequest,
+  body('term')
+    .notEmpty().withMessage(requiredMessage).bail()
+    .isLength({ min: 2 }).withMessage(fieldSizeMessage(2)),
+  body('answer')
+    .notEmpty().withMessage(requiredMessage).bail()
+    .isLength({ min: 5 }).withMessage(fieldSizeMessage(5))
+]
+
+export const UpdateDefinitionRequest = [
   body('term')
     .notEmpty().withMessage(requiredMessage).bail()
     .isLength({ min: 2 }).withMessage(fieldSizeMessage(2)),
