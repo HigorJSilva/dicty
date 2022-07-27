178eb816c3eb820fceceb1673c40d27b41ca0862
Higor Silva <higor.junio@hotmail.com>
2022-07-27 13:41:12 -0300

    feat: add search term route

--- src/routes/DictionaryRoutes.ts
+++ src/routes/DictionaryRoutes.ts
@@ -1,13 +1,14 @@
 import { authorize } from '@/middlewares/Auth'
-import { AddDefinitionRequest, ApproveUserTermRequest, DeleteDefinitionRequest, UpdateDefinitionRequest, UserAnswerRequest } from '@/middlewares/Requests/DictionaryRequests'
+import { AddDefinitionRequest, ApproveUserTermRequest, DeleteDefinitionRequest, SearchDefinitionRequest, UpdateDefinitionRequest, UserAnswerRequest } from '@/middlewares/Requests/DictionaryRequests'
 import { validateRequest } from '@/middlewares/ValidateRequest'
 import express from 'express'
-import { store, list, update, remove, userAnswer, userDefiniton, userDefinitonApproval } from '../controllers/DictionaryController'
+import { store, list, update, remove, userAnswer, userDefiniton, userDefinitonApproval, search } from '../controllers/DictionaryController'
 
 const router = express.Router()
 
 router.get('/', authorize(), list)
 router.post('/', authorize('admin'), AddDefinitionRequest, validateRequest, store)
+router.post('/search', authorize(), SearchDefinitionRequest, validateRequest, search)
 router.put('/:termId', authorize('admin'), UpdateDefinitionRequest, validateRequest, update)
 router.delete('/:termId', authorize('admin'), DeleteDefinitionRequest, validateRequest, remove)
 
